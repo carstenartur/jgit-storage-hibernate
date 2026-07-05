e6dfb74fbf9955ce8898f60e3f6d89e3268fee9e
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-05 10:40:09 +0200

    Add core public package documentation

---
+++ jgit-storage-hibernate-core/src/main/java/io/github/carstenartur/jgit/storage/hibernate/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2026, Carsten Hammer and contributors.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+/**
+ * Public API for Hibernate-backed JGit storage.
+ *
+ * <p>Consuming projects should use this package only. Implementation packages encapsulate JGit
+ * DFS/Reftable internals and are not part of the stable API.
+ */
+package io.github.carstenartur.jgit.storage.hibernate;
