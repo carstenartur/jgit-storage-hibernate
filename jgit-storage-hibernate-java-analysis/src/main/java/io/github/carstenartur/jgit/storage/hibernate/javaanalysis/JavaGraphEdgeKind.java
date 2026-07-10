7d8f765dd5276251fd70d133be7e5279fee0d6c9
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-10 18:55:09 +0200

    Add Java graph edge kinds

---
+++ jgit-storage-hibernate-java-analysis/src/main/java/io/github/carstenartur/jgit/storage/hibernate/javaanalysis/JavaGraphEdgeKind.java
@@ -0,0 +1,15 @@
+/* Copyright (C) 2026, Carsten Hammer and contributors. SPDX-License-Identifier: BSD-3-Clause */
+package io.github.carstenartur.jgit.storage.hibernate.javaanalysis;
+
+/** Semantic relations represented in a versioned Java software graph. */
+public enum JavaGraphEdgeKind {
+  CALLS,
+  CONSTRUCTS,
+  READS_FIELD,
+  WRITES_FIELD,
+  REFERENCES_TYPE,
+  EXTENDS,
+  IMPLEMENTS,
+  OVERRIDES,
+  ANNOTATED_WITH
+}
