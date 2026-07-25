d415b9be530cf0be35d719926ef15d8c36f77aff
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-25 20:42:50 +0200

    Make nationalized reflog messages portable to Oracle (#86)
    Cap the nationalized reflog message mapping at the Oracle-portable 2,000-character boundary and guard the mapping with a regression test.

---
+++ jgit-storage-hibernate-core/src/test/java/io/github/carstenartur/jgit/storage/hibernate/GitReflogEntityMappingTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2026, Carsten Hammer and contributors.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-Clause License.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package io.github.carstenartur.jgit.storage.hibernate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.github.carstenartur.jgit.storage.hibernate.entity.GitReflogEntity;
+import jakarta.persistence.Column;
+import java.lang.reflect.Field;
+import org.hibernate.annotations.Nationalized;
+import org.junit.jupiter.api.Test;
+
+class GitReflogEntityMappingTest {
+
+  @Test
+  void reflogMessageUsesOraclePortableNationalizedLength() throws Exception {
+    Field message = GitReflogEntity.class.getDeclaredField("message");
+    Column column = message.getAnnotation(Column.class);
+
+    assertNotNull(message.getAnnotation(Nationalized.class));
+    assertNotNull(column);
+    assertEquals(2000, GitReflogEntity.MAX_MESSAGE_LENGTH);
+    assertEquals(GitReflogEntity.MAX_MESSAGE_LENGTH, column.length());
+  }
+}
