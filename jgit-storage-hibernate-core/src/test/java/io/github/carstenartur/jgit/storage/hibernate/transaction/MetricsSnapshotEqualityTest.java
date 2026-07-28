8f090493cc85b65c4dd35aada6e865691189f074
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-29 00:29:23 +0200

    Cover metrics snapshot value semantics

---
+++ jgit-storage-hibernate-core/src/test/java/io/github/carstenartur/jgit/storage/hibernate/transaction/MetricsSnapshotEqualityTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2026, Carsten Hammer and contributors.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-Clause License.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package io.github.carstenartur.jgit.storage.hibernate.transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MetricsSnapshotEqualityTest {
+
+  @Test
+  void recordUsesValueEquality() {
+    assertEquals(
+        new StorageOperationMetrics(1, 2, 3, 4, 5),
+        new StorageOperationMetrics(1, 2, 3, 4, 5));
+  }
+}
