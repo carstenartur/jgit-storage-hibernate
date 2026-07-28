4d58fa0f650cb7af2c60cfe1c901dff869b2a3cd
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-29 00:30:14 +0200

    Cover metrics property constant

---
+++ jgit-storage-hibernate-core/src/test/java/io/github/carstenartur/jgit/storage/hibernate/transaction/MetricsConstantTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2026, Carsten Hammer and contributors.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the BSD 3-Clause License.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package io.github.carstenartur.jgit.storage.hibernate.transaction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MetricsConstantTest {
+
+  @Test
+  void propertyNameIsStable() {
+    assertEquals(
+        "jgit.storage.hibernate.metrics.enabled",
+        HibernateTransactionContext.METRICS_ENABLED_PROPERTY);
+  }
+}
