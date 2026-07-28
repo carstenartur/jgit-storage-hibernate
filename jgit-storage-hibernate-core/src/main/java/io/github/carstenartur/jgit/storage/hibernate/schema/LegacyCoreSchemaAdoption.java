f6384e84caad6f403b69bc7d0f2afb05224f855c
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-28 21:14:44 +0200

    Make legacy schema preflight SQL portable

--- jgit-storage-hibernate-core/src/main/java/io/github/carstenartur/jgit/storage/hibernate/schema/LegacyCoreSchemaAdoption.java
+++ jgit-storage-hibernate-core/src/main/java/io/github/carstenartur/jgit/storage/hibernate/schema/LegacyCoreSchemaAdoption.java
@@ -149,11 +149,18 @@ public final class LegacyCoreSchemaAdoption {
   }
 
   private static long countOverlongPackExtensionRows(Connection connection) {
-    try {
-      return count(
-          connection,
-          "select count(*) from git_packs where character_length(pack_extension) > "
-              + CORE_PACK_EXTENSION_LENGTH);
+    long overlongRows = 0;
+    try (Statement statement = connection.createStatement();
+        ResultSet resultSet =
+            statement.executeQuery(
+                "select pack_extension from git_packs where pack_extension is not null")) {
+      while (resultSet.next()) {
+        String extension = resultSet.getString(1);
+        if (extension.codePointCount(0, extension.length()) > CORE_PACK_EXTENSION_LENGTH) {
+          overlongRows++;
+        }
+      }
+      return overlongRows;
     } catch (SQLException exception) {
       throw new LegacyCoreSchemaAdoptionException(
           "Could not validate existing git_packs pack_extension values", exception);
