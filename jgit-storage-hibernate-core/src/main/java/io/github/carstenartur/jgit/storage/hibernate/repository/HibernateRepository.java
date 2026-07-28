8ac4d09a2b8d67ddb80e73dd8ea47c423c944b28
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-28 22:14:09 +0200

    Use read-ahead object database

--- jgit-storage-hibernate-core/src/main/java/io/github/carstenartur/jgit/storage/hibernate/repository/HibernateRepository.java
+++ jgit-storage-hibernate-core/src/main/java/io/github/carstenartur/jgit/storage/hibernate/repository/HibernateRepository.java
@@ -10,6 +10,7 @@ package io.github.carstenartur.jgit.storage.hibernate.repository;
 
 import io.github.carstenartur.jgit.storage.hibernate.entity.GitRepositoryLockEntity;
 import io.github.carstenartur.jgit.storage.hibernate.objects.HibernateObjDatabase;
+import io.github.carstenartur.jgit.storage.hibernate.objects.ReadAheadHibernateObjDatabase;
 import io.github.carstenartur.jgit.storage.hibernate.refs.HibernateRefDatabase;
 import io.github.carstenartur.jgit.storage.hibernate.refs.HibernateReflogReader;
 import io.github.carstenartur.jgit.storage.hibernate.refs.HibernateReflogWriter;
@@ -45,7 +46,7 @@ public class HibernateRepository extends DfsRepository {
     this.repositoryName = builder.getRepositoryName();
     this.transactionContext = new HibernateTransactionContext(sessionFactory);
     this.objectDatabase =
-        new HibernateObjDatabase(
+        new ReadAheadHibernateObjDatabase(
             this,
             builder.getReaderOptions(),
             sessionFactory,
