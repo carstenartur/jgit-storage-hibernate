a6fc995ac9b5f77734e492965e3048d60ef39599
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-29 18:27:50 +0200

    Add SQL Server reverse reflog index

---
+++ jgit-storage-hibernate-core/src/main/resources/db/migration/jgit-storage-hibernate/core/sqlserver/V0_1_17__optimize_reverse_reflog_reads.sql
@@ -0,0 +1,6 @@
+-- ref_name is nvarchar(1024) and cannot be part of a 1700-byte SQL Server index key.
+-- Keep it as an included residual predicate while repository/id provide newest-first access.
+drop index idx_reflog_repo_ref on git_reflog;
+create index idx_reflog_repo_id
+    on git_reflog (repository_name, id desc)
+    include (ref_name);
