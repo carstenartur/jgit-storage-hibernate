10e67287d9e45108bd96a19b52b33b1e9df74b09
Carsten Hammer <carsten.hammer@t-online.de>
2026-07-29 18:27:31 +0200

    Add PostgreSQL reverse reflog index

---
+++ jgit-storage-hibernate-core/src/main/resources/db/migration/jgit-storage-hibernate/core/postgresql/V0_1_17__optimize_reverse_reflog_reads.sql
@@ -0,0 +1,4 @@
+-- Cover repository/ref filtering and newest-first reflog reads with one ordered index.
+drop index idx_reflog_repo_ref;
+create index idx_reflog_repo_ref_id
+    on git_reflog (repository_name, ref_name, id desc);
